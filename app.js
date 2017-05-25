1aa113833039630ba1b16d4d2dbaa07d1394f9c9
Gihwan Kim <0x1306e6d@gmail.com>
2017-05-25 22:55:59 +0900

    Initialize middleware
    Initialize express-session
    Initialize passport
    Initialize connect-flash

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +24,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'descrify',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use('/', index);
 app.use('/users', users);
