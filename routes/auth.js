cc2255a2635392014aaa6e4b22034afdd244244c
Gihwan Kim <0x1306e6d@gmail.com>
2017-05-25 23:32:57 +0900

    Use passport as login/join
    Use 'local-login' strategy as login
    Use 'local-join' strategy as join

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 router.get('/', function (req, res) {
@@ -17,6 +18,12 @@ router.get('/login', function (req, res) {
     }
 });
 
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/',
+    failureRedirect: '/auth/login',
+    failureFlash: true
+}));
+
 router.get('/join', function (req, res) {
     if (req.isAuthenticated()) {
         res.redirect('/');
@@ -24,5 +31,11 @@ router.get('/join', function (req, res) {
         res.render('join');
     }
 });
+
+router.post('/join', passport.authenticate('local-join', {
+    successRedirect: '/auth/login',
+    failureRedirect: '/auth/join',
+    failureFlash: true
+}));
 
 module.exports = router;
