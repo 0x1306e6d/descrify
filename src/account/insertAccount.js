82e3cd70508032f1e81de14ed029ea4db6ed3111
Gihwan Kim <0x1306e6d@gmail.com>
2017-05-26 00:24:40 +0900

    Implement login/join
    Implement login
    Implement join
    Implement find by email
    Implement find by username
    Implement insert new account

---
+++ src/account/insertAccount.js
@@ -0,0 +1,34 @@
+var mariadb = require('../database/mariadb');
+
+module.exports = function (user, callback) {
+    mariadb.get(function (err, con) {
+        if (err) {
+            if (con) {
+                con.release();
+            }
+            console.log("Failed to get mariadb.");
+
+            return callback(err);
+        }
+
+        var sql = "INSERT INTO account SET ?";
+        var data = {
+            email: user.email,
+            password: user.password,
+            username: user.username
+        };
+
+        var exec = con.query(sql, data, function (err, result) {
+            if (con) {
+                con.release();
+            }
+            console.log("[insertAccount] SQL : " + exec.sql);
+
+            if (err) {
+                callback(err);
+            } else {
+                callback(null);
+            }
+        });
+    });
+};
--- src/auth/local/join.js
+++ src/auth/local/join.js
@@ -1,5 +1,40 @@
 var LocalStrategy = require('passport-local').Strategy;
 
+var findEmail = require('../../account/findEmail');
+var findUsername = require('../../account/findUsername');
+var insertAccount = require('../../account/insertAccount');
+
+function doJoin(user, callback) {
+    findEmail(user.email, function (err, exists) {
+        if (err) {
+            console.log("Failed to find account. email=" + user.email);
+            return callback(err);
+        }
+        if (exists) {
+            return callback(null, null);
+        }
+
+        findUsername(user.username, function (err, exists) {
+            if (err) {
+                console.log("Failed to find username. username=" + user.username);
+                return callback(err);
+            }
+            if (exists) {
+                return callback(null, null);
+            }
+
+            insertAccount(user, function (err) {
+                if (err) {
+                    console.log("Failed to insert account. email=" + user.email);
+                    return callback(err);
+                } else {
+                    callback(null, user);
+                }
+            });
+        });
+    });
+}
+
 module.exports = new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
@@ -7,10 +42,24 @@ module.exports = new LocalStrategy({
 }, function (req, email, password, done) {
     var user = {
         email: email,
-        password: password
+        password: password,
+        username: req.body.username
     };
 
-    // TODO: implement join
+    doJoin(user, function (err, user) {
+        if (err) {
+            console.log("Failed to join account.");
+            console.error(err);
 
-    return done(null, user);
+            return done(err);
+        }
+        if (user) {
+            console.log("New account is joined");
+            console.dir(user);
+
+            return done(null, user);
+        } else {
+            return done(null, false, req.flash('joinMessage', "회원 가입을 실패하였습니다."));
+        }
+    });
 });
