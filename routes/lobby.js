d5bcbb890f8d81b2c0bf1acd45ed368e78101ba8
Gihwan Kim <0x1306e6d@gmail.com>
2017-05-27 18:35:35 +0900

    Show all games at lobby page

--- routes/lobby.js
+++ routes/lobby.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var Game = require('../src/database/game');
 var router = express.Router();
 
 router.get('/', function (req, res) {
     if (req.isAuthenticated()) {
-        res.render('lobby');
+        Game.findAll(function (err, games) {
+            if (err) {
+                console.log("Failed to find all games");
+                console.error(err);
+
+                res.status(500).end();
+            } else {
+                res.render('lobby', {
+                    games: games
+                });
+            }
+        });
     } else {
         res.redirect('/auth/login');
     }
