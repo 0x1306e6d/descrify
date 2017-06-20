6916fed4a08692a9ff0e55eee068c9c17e6795a0
Gihwan Kim <0x1306e6d@gmail.com>
2017-06-20 14:29:17 +0900

    Get game list in lobby page using socket.io

--- routes/lobby.js
+++ routes/lobby.js
@@ -1,19 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const Game = require('../src/database/game');
-
 router.get('/', function (req, res) {
-    Game.findAll(function (err, games) {
-        if (err) {
-            console.error("Failed to find all games.", err);
-            return res.status(500).end();
-        }
-
-        res.render('lobby', {
-            games: games
-        });
-    });
+    res.render('lobby');
 });
 
 module.exports = router;
